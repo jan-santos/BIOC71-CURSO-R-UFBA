f1d0f68a48715bcdf651a74bcd8707deb6d243ce
jan-santos <jan-santos@hotmail.com>
2017-12-12 17:10:04 -0300

    linha 15 a 21 alterada

--- aula_0.R
+++ aula_0.R
@@ -12,13 +12,7 @@
 # o que é um dataframe? Qual a diferneça entre um dataframe e um list?
 #data.frame é uma tabela (coluna e linhas), matriz para valores de classes diferentes, e lista é um conjunto de objetos
 
-# o que as funções abaixo fazem? O que elas retornam? #funcoes com is na frente eh uma pergunta
-is.vector() #responde se é vector. Retorna: TRUE or FALSE
-is.numeric() #responde se é objeto do tipo numérico. Retorna: TRUE or FALSE 
-is.character() # responde se é objeto do tipo character. Retorna: TRUE or FALSE
-is.matrix(peso) # pergunta se o objeto peso eh uma matrix
-is.na() # verifica se é NA.Retorna: TRUE or FALSE
-is.data.frame() # verifica se é data.frame.Retorna: TRUE or FALSE
+1+2+3
 
 
 #o que queremos dizer quando dizemos uma função "retorna" alguma coisa?
